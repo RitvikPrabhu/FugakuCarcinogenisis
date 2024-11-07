a8f4d1b162e667f772cecad8ef5c7fc03772ef94
Ritvik Prabhu <ritvikp@vt.edu>
2024-11-07 17:22:27 +0900

    4 hit pruning done; agrees with both conditions; works on small dataset - need to test on larger dataset

--- graphSparsity.cpp
+++ graphSparsity.cpp
@@ -8,25 +8,49 @@
 
 #define MAX_BUF_SIZE 1024
 
-struct Node {
-	bool oneTumor;
-	bool noNormal;
+struct Gene {
+	std::set<int> tumor;
+	std::set<int> normal;
 };
 
 
-void funcName(std::vector<std::set<int> > data, int totalGenes, int numTumor, int numNormal){
+void funcName(std::vector<Gene> data, int totalGenes, int numTumor, int numNormal){
 	for (int i = 0; i < totalGenes; i++){
-		std::set<int> gene1 = data[i];
+		Gene gene1 = data[i];
 		for (int j = i + 1; j < totalGenes; j++){
-			std::set<int> gene2 = data[j];
-			std::set<int> intersect;
-
-			std::set_intersection(gene1.begin(), gene1.end(), gene2.begin(), gene2.end(),
-                        std::inserter(intersect, intersect.begin()));
+			Gene gene2 = data[j];
+			std::set<int> intersectNormal1, intersectTumor1;
+		
+			std::set_intersection(gene1.tumor.begin(), gene1.tumor.end(), gene2.tumor.begin(), gene2.tumor.end(), std::inserter(intersectTumor1, intersectTumor1.begin()));	
+			std::set_intersection(gene1.normal.begin(), gene1.normal.end(), gene2.normal.begin(), gene2.normal.end(), std::inserter(intersectNormal1, intersectNormal1.begin()));
 			
-			for (int k = j + 1; k < totalGenes; k++){
-				printf("Combination: %d %d %d\n", i, j, k);
+			if (!intersectTumor1.empty()) {
+				printf("HELLLLLOOOO\n");
+				for (int k = j + 1; k < totalGenes; k++){
+					
+					Gene gene3 = data[k];
+					std::set<int> intersectNormal2, intersectTumor2;
+					std::set_intersection(gene3.tumor.begin(), gene3.tumor.end(), intersectTumor1.begin(), intersectTumor1.end(), std::inserter(intersectTumor2, intersectTumor2.begin()));	
+					std::set_intersection(gene3.normal.begin(), gene3.normal.end(), intersectNormal1.begin(), intersectNormal1.end(), std::inserter(intersectNormal2, intersectNormal2.begin()));
+
+					if (!intersectTumor2.empty()){
+
+						for (int l = k + 1; l < totalGenes; l++){
+							Gene gene4 = data[l];
+							std::set<int> intersectNormal3, intersectTumor3;
+							
+							std::set_intersection(gene4.tumor.begin(), gene4.tumor.end(), intersectTumor2.begin(), intersectTumor2.end(), std::inserter(intersectTumor3, intersectTumor3.begin()));	
+							std::set_intersection(gene4.normal.begin(), gene4.normal.end(), intersectNormal2.begin(), intersectNormal2.end(), std::inserter(intersectNormal3, intersectNormal3.begin()));
+							if (!intersectTumor3.empty() && intersectNormal3.empty()){
+							
+								printf("Combination: %d %d %d %d\n", i, j, k, l);
+							}
+							
+						}
+					}
+				}
 			}
+
 		
 		}
 	
@@ -59,7 +83,7 @@ int main(int argc, char *argv[]){
 
 	printf("%d %d %d %d %d\n", numGenes, numSamples, value, numTumor, numNormal); 
 
-	std::vector<std::set<int> > sparseData (numGenes);
+	std::vector<Gene> sparseData (numGenes);
 
 	int fileRows = numGenes * numSamples;
 
@@ -70,14 +94,16 @@ int main(int argc, char *argv[]){
 			fclose(dataFile);
 			return 1; 
 		}
-		
+	
 		if (value > 0){
-			sparseData[gene].insert(sample);
+			if (sample < numTumor && value > 0){
+				sparseData[gene].tumor.insert(sample);
+			}
+			else{
+				sparseData[gene].normal.insert(sample);
+			}
 		}
 	}
 
 	funcName(sparseData, numGenes, numTumor, numNormal);
-
-
-
 }
