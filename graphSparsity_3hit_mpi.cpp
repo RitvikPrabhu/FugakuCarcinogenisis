eaed9d79b2403d6336d439c91c809cbc9d608967
RitvikPrabhu <ritvik@ritvikprabhu.com>
2024-11-12 15:14:35 +0900

    mpi time measure

--- graphSparsity_3hit_mpi.cpp
+++ graphSparsity_3hit_mpi.cpp
@@ -29,7 +29,7 @@ long long int nCr(int n, int r) {
     return result;
 }
 
-void funcName(std::vector<Gene> data, int totalGenes, int numTumor, int numNormal, long long int &count, int rank, int size, std::ofstream &outFile){
+void funcName(std::vector<Gene> data, int totalGenes, int numTumor, int numNormal, long long int &count, int rank, int size){
 	
 	long long int num_Comb, startComb, endComb, chunkSize, remainder;
  	num_Comb = nCr(totalGenes, 2);	
@@ -59,10 +59,8 @@ void funcName(std::vector<Gene> data, int totalGenes, int numTumor, int numNorma
 							std::set_intersection(gene3.tumor.begin(), gene3.tumor.end(), intersectTumor1.begin(), intersectTumor1.end(), std::inserter(intersectTumor2, intersectTumor2.begin()));
 					std::set_intersection(gene3.normal.begin(), gene3.normal.end(), intersectNormal1.begin(), intersectNormal1.end(), std::inserter(intersectNormal2, intersectNormal2.begin()));
 					
-					if (!intersectTumor2.empty() && intersectNormal2.empty()){
+					if (!intersectTumor2.empty()){
 						count++;
-						//printf("(%lld %lld %lld)\n", count, i, j, k);
-						outFile << "("  << i << " " << j << " " << k << ")\n";
 					}
 				}
 
@@ -78,13 +76,15 @@ int main(int argc, char *argv[]){
 	int rank, size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+	double total_start_time = MPI_Wtime();
 	if (argc != 2){
 		printf("One argument expected: ./graphSparcity <dataFile>");
 		MPI_Finalize();
 		return 1;
 	}
-
+	double start_time, end_time;
+    	double elapsed_time_loading, elapsed_time_func, elapsed_time_total;	
+	start_time = MPI_Wtime();
 	FILE* dataFile;
 	dataFile = fopen(argv[1], "r");
 	
@@ -127,29 +127,62 @@ int main(int argc, char *argv[]){
 	}
 	
 	fclose(dataFile);
-	
-	std::ofstream outFile;
-	outFile.open("combinations_rank_" + std::to_string(rank) + ".txt");
-	if (!outFile.is_open()) {
-		printf("Error opening output file for rank %d\n", rank);
-		MPI_Finalize();
-		return 1;
-	}
+	end_time = MPI_Wtime();
+	elapsed_time_loading = end_time - start_time;
+
 	long long int count = 0;
+	start_time = MPI_Wtime();
 	//auto start = std::chrono::high_resolution_clock::now();
-	funcName(sparseData, numGenes, numTumor, numNormal, count, rank, size, outFile);
-	//auto end = std::chrono::high_resolution_clock::now();
-//	std::chrono::duration<double, std::milli> duration = end - start;
-        //printf("Time taken: %.3f ms\n", duration.count());
-        outFile.close();
+	funcName(sparseData, numGenes, numTumor, numNormal, count, rank, size);
+	
+	double total_end_time = MPI_Wtime();
+    	elapsed_time_total = total_end_time - total_start_time;
+	end_time = MPI_Wtime();
+    	elapsed_time_func = end_time - start_time;
+	double elapsed_times[3] = {elapsed_time_loading, elapsed_time_func, elapsed_time_total};
+    	double all_times[3][size];
+	MPI_Gather(elapsed_times, 3, MPI_DOUBLE, all_times, 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 	long long int totalCount = 0;
     	MPI_Reduce(&count, &totalCount, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 	
 	if (rank == 0) {
-		printf("Total number of combinations: %lld\n", totalCount);
-	}
+		std::ofstream timingFile("output.txt");
+		if (timingFile.is_open()) {
+		    for (int stage = 0; stage < 3; ++stage) {
+			double max_time = -1.0, min_time = 1e9, total_time = 0.0;
+			int rank_max = 0, rank_min = 0;
+			for (int i = 0; i < size; ++i) {
+			    double time = all_times[stage][i];
+			    if (time > max_time) {
+				max_time = time;
+				rank_max = i;
+			    }
+			    if (time < min_time) {
+				min_time = time;
+				rank_min = i;
+			    }
+			    total_time += time;
+			}
+			double avg_time = total_time / size;
+			if (stage == 0) {
+			    timingFile << "Stage " << stage << " (Loading Data):\n";
+			} else if (stage == 1) {
+			    timingFile << "Stage " << stage << " (Computation):\n";
+			} else {
+			    timingFile << "Stage " << stage << " (Total Time):\n";
+			}
+			timingFile << "Rank " << rank_max << " took the longest time: " << max_time << " seconds.\n";
+			timingFile << "Rank " << rank_min << " took the shortest time: " << min_time << " seconds.\n";
+			timingFile << "Average time: " << avg_time << " seconds.\n\n";
+		    }
+		    timingFile << "Total number of combinations: " << totalCount << "\n";
+		    timingFile.close();
+		} else {
+		    printf("Error opening timings output file\n");
+		}
+    }
 
 	MPI_Finalize();
-
+	return 0;
 }
