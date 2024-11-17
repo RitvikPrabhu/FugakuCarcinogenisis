0f1aca40d15ecca1ebdbddeb4dd079865d38492b
RitvikPrabhu <ritvik@ritvikprabhu.com>
2024-11-17 21:59:47 +0900

    Fugaku submit script simplified

--- runScripts/fsub.sh
+++ runScripts/fsub.sh
@@ -5,7 +5,4 @@
 #PJM --mpi proc=192
 #PJM -x PJM_LLIO_GFSCACHE=/vol0004
 
-
-mpiFCC -Nclang -std=c++11 -Ofast -o graphSparsity_3hit_mpi graphSparsity_3hit_mpi.cpp
-mpirun ./graphSparsity_3hit_mpi sample.txt
-find . -type f -name "combinations_rank_*" -size +0 -print
+./compileRun.sh
