7136d7f7693465fd9005d7f00dc69bda94cd434d
Emil VATAI <emil.vatai@gmail.com>
2024-11-21 13:39:23 -0500

    Add comments

--- mpi_scan_test.c
+++ mpi_scan_test.c
@@ -6,8 +6,18 @@ int main(int argc, char *argv[]) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int input, output;
-  input = rank + 1;
+  if (rank) {
+    // rank>0 will use 10*rank as the input.
+    input = 10 * rank;
+  } else {
+    // rank == 0 will use 8 (i.e. sizeof(unsigned long long))
+    input = 8;
+  }
   MPI_Exscan(&input, &output, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  if (rank == 0) {
+    // The result for rank == 0 must be explicitly set!
+    output = 0;
+  }
   printf("rank: %d; result: %d\n", rank, output);
   MPI_Finalize();
   return 0;
