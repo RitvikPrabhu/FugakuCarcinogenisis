355056127dea45db4855dd6f9b4b9f4a5cc33614
Emil VATAI <emil.vatai@gmail.com>
2024-11-27 03:42:44 +0900

    Get number of triplets

--- src/4hits.c
+++ src/4hits.c
@@ -1,9 +1,27 @@
 #include <mpi.h>
 #include <stdio.h>
 
+const char *PRUNED_3HIT_TRIPLETS = "/home/vatai/3hit_pruned_ACC_data_400.bin";
+
+size_t get_num_triplets(const char *filename) {
+  MPI_File file;
+  MPI_Status status;
+  size_t num_triplets;
+  int err = MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY,
+                          MPI_INFO_NULL, &file);
+  MPI_File_read_all(file, &num_triplets, 1, MPI_LONG, &status);
+  MPI_File_close(&file);
+  return num_triplets;
+}
+
 int main(int argc, char *argv[]) {
-  MPI_Init(&argc, &argc);
-  printf("Hello\n");
+  MPI_Init(&argc, &argv);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0)
+    printf("Hello\n");
+  size_t num_triplets = get_num_triplets(PRUNED_3HIT_TRIPLETS);
+  printf("num_triplets = %lu\n", num_triplets);
   MPI_Finalize();
   return 0;
 }
