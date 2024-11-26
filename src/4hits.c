d15e44ea335118eb0878a872105ea3b64ec49096
Emil VATAI <emil.vatai@gmail.com>
2024-11-27 06:26:39 +0900

    Copy calculate indices

--- src/4hits.c
+++ src/4hits.c
@@ -1,6 +1,6 @@
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 #include <mpi.h>
 
@@ -18,6 +18,23 @@ size_t get_num_triplets(const char *filename) {
   MPI_File_read_all(file, &num_triplets, 1, MPI_LONG, &status);
   MPI_File_close(&file);
   return num_triplets;
+}
+
+struct interval_t {
+  int64_t begin;
+  int64_t end;
+};
+
+struct interval_t calculate_indices(MPI_Comm comm, int64_t total_count) {
+  struct interval_t interval;
+  int rank, size;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+  int64_t per_process = total_count / size;
+  int64_t remainder = total_count % size;
+  interval.begin = rank * per_process + (rank < remainder ? rank : remainder);
+  interval.end = interval.begin + per_process + (rank < remainder ? 1 : 0);
+  return interval;
 }
 
 int main(int argc, char *argv[]) {
