e7a38e6e36be4e52c182f19a9f5b0f1b5a65eacb
Emil VATAI <emil.vatai@gmail.com>
2024-11-27 06:55:43 +0900

    Clean up

--- src/4hits.c
+++ src/4hits.c
@@ -8,6 +8,16 @@
 
 const char *PRUNED_3HIT_TRIPLETS = "/home/vatai/3hit_pruned_ACC_data_400.bin";
 const char *DB_FILE = "/home/vatai/ACC.combinedData.txt";
+const size_t CHUNK_SIZE = 1 << 15;
+
+size_t serial_num_triplets(const char *filename) {
+  FILE *file;
+  size_t num_triplets;
+  file = fopen(filename, "r");
+  fread(&num_triplets, sizeof(num_triplets), 1, file);
+  fclose(file);
+  return num_triplets;
+}
 
 size_t get_num_triplets(const char *filename) {
   MPI_File file;
@@ -20,21 +30,16 @@ size_t get_num_triplets(const char *filename) {
   return num_triplets;
 }
 
-struct interval_t {
-  int64_t begin;
-  int64_t end;
-};
+void master() {
+  printf("Hello\n");
+  size_t num_triplets = serial_num_triplets(PRUNED_3HIT_TRIPLETS);
+  printf("num_triplets: %lu\n", num_triplets);
+}
 
-struct interval_t calculate_indices(MPI_Comm comm, int64_t total_count) {
-  struct interval_t interval;
-  int rank, size;
-  MPI_Comm_rank(comm, &rank);
-  MPI_Comm_size(comm, &size);
-  int64_t per_process = total_count / size;
-  int64_t remainder = total_count % size;
-  interval.begin = rank * per_process + (rank < remainder ? rank : remainder);
-  interval.end = interval.begin + per_process + (rank < remainder ? 1 : 0);
-  return interval;
+void worker() {
+  struct db_t tumor, normal;
+  // if (rank == 0)
+  get_db(DB_FILE, &tumor, &normal);
 }
 
 int main(int argc, char *argv[]) {
@@ -43,17 +48,10 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   if (rank == 0)
-    printf("Hello\n");
+    master();
+  else
+    worker();
 
-  size_t num_triplets = get_num_triplets(PRUNED_3HIT_TRIPLETS);
-  /* printf("num_triplets = %lu\n", num_triplets); */
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  struct db_t tumor, normal;
-  // if (rank == 0)
-  get_db(DB_FILE, &tumor, &normal);
-  if (rank == 0)
-    printf("Done\n");
   MPI_Finalize();
   return 0;
 }
