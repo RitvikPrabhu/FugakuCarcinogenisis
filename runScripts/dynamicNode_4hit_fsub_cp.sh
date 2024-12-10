1611655950a0fb14ee216bc91de4a09bfe6e6708
RitvikPrabhu <ritvik@ritvikprabhu.com>
2024-12-10 10:49:41 +0900

    WIP

--- runScripts/dynamicNode_4hit_fsub_cp.sh
+++ runScripts/dynamicNode_4hit_fsub_cp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Dynamic node counts for multiple jobs
 
-NODE_COUNTS=( 8000 )
+NODE_COUNTS=( 500 )
 
 for NODE_COUNT in "${NODE_COUNTS[@]}"; do
     RUN_SCRIPT="run_${NODE_COUNT}.sh"
@@ -11,15 +11,14 @@ for NODE_COUNT in "${NODE_COUNTS[@]}"; do
 #PJM -g ra000012
 #PJM -N ${JOB_NAME}
 #PJM -L node=${NODE_COUNT}
-#PJM -L elapse=02:00:00
+#PJM -L elapse=05:00:00
 #PJM --mpi proc=${NODE_COUNT}
 #PJM -x PJM_LLIO_GFSCACHE=/vol0004
 #PJM -L "rscgrp=large"
-llio-transfer ../data/ACC.combinedData.txt
+llio_transfer ../data/ACC.combinedData.txt
 ./submit_4hit.sh ACC.combinedData.txt metrics_${NODE_COUNT}.txt results_${NODE_COUNT}.txt ${NODE_COUNT}
 EOF
     chmod +x "$RUN_SCRIPT"
     pjsub "$RUN_SCRIPT"
 done
 
-mv output.* outputs
