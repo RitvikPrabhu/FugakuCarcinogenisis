60b7ed3f950021c43b20856461e08da8023da993
RitvikPrabhu <ritvik@ritvikprabhu.com>
2024-12-17 14:54:00 +0900

    [STABLE] All half node run scripts updated

--- runScripts/dynamicNode_5hit_omp_ACC_fsub.sh
+++ runScripts/dynamicNode_5hit_omp_ACC_fsub.sh
@@ -4,23 +4,23 @@
 NODE_COUNTS=( 82944 )
 
 for NODE_COUNT in "${NODE_COUNTS[@]}"; do
-    RUN_SCRIPT="4hit_run_${NODE_COUNT}.sh"
-	JOB_NAME="4hit_job_${NODE_COUNT}"
+    RUN_SCRIPT="5hit_run_${NODE_COUNT}_omp_ACC.sh"
+	JOB_NAME="5hit_job_${NODE_COUNT}_omp_ACC"
     cat <<EOF > "$RUN_SCRIPT"
 #!/bin/bash
 #PJM -g ra000012
 #PJM -N ${JOB_NAME}
 #PJM -L node=${NODE_COUNT}
 #PJM -L elapse=02:00:00
-#PJM --mpi proc=3981312
+#PJM --mpi proc=${NODE_COUNT}
 #PJM -x PJM_LLIO_GFSCACHE=/vol0004
 #PJM -L "rscgrp=huge"
 #PJM -m b,e
 #PJM --mail-list ritvikp@vt.edu
-
+export OMP_NUM_THREADS=48
 llio_transfer ../data/ACC.combinedData.txt
-llio_transfer "bin/dataSparsity_4hit"
-mpirun "bin/dataSparsity_4hit" ../data/ACC.combinedData.txt 4hit_metrics_${NODE_COUNT}.txt 4hit_${NODE_COUNT}.out
+llio_transfer "bin/dataSparsity_5hit_omp"
+mpirun "bin/dataSparsity_5hit_omp" ../data/ACC.combinedData.txt 5hit_metrics_${NODE_COUNT}_omp_ACC.txt 5hit_${NODE_COUNT}_omp_ACC.out
 
 EOF
     chmod +x "$RUN_SCRIPT"
