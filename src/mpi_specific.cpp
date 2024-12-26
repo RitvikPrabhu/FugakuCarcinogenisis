235720d91e4ae2bac82fd00f76b242cdec3bfd18
Ritvik Prabhu <ritvikp@vt.edu>
2024-12-26 10:48:56 +0900

    Remove commented out code chunk since implementation already works

--- src/mpi_specific.cpp
+++ src/mpi_specific.cpp
@@ -53,27 +53,3 @@ void master_process(int num_workers, long long int num_Comb) {
   distribute_work(num_workers, num_Comb, next_idx);
   send_termination_signals(num_workers);
 }
-/*
-void master_process(int num_workers, long long int num_Comb) {
-  long long int next_idx = num_workers * CHUNK_SIZE;
-  while (next_idx < num_Comb) {
-    MPI_Status status;
-    int flag;
-    MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
-
-    if (flag == 1) {
-      char c;
-      int workerRank = status.MPI_SOURCE;
-      MPI_Recv(&c, 1, MPI_CHAR, workerRank, 1, MPI_COMM_WORLD, &status);
-      if (c == 'a') {
-        MPI_Send(&next_idx, 1, MPI_LONG_LONG_INT, workerRank, 2,
-                 MPI_COMM_WORLD);
-        next_idx += CHUNK_SIZE;
-      }
-    }
-  }
-  for (int workerRank = 1; workerRank <= num_workers; ++workerRank) {
-    long long int term_signal = -1;
-    MPI_Send(&term_signal, 1, MPI_LONG_LONG_INT, workerRank, 2, MPI_COMM_WORLD);
-  }
-}*/
