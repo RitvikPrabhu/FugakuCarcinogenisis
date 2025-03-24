0f1db063d70e54d0299fc56232c67f5d83502c88
RitvikPrabhu <ritvik@ritvikprabhu.com>
2025-03-25 08:46:46 +0900

    Fugaku compile command

--- build/compile.sh
+++ build/compile.sh
@@ -2,4 +2,4 @@
 mpiFCCpx -Kparallel -Kfast -std=c++11 -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_bit
 
 #CPP set
-mpiFCCpx -Kparallel -Kfast -std=c++11 -DUSE_CPP_SET=ON -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_bit
+mpiFCCpx -Kparallel -Kfast -std=c++11 -DUSE_CPP_SET -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_set
