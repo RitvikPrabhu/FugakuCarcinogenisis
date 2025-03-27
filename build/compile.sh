24a6d60fbc1e2320260f6f343a88d88c3e85b606
RitvikPrabhu <ritvik@ritvikprabhu.com>
2025-03-27 12:56:30 +0900

    Compile with timing info

--- build/compile.sh
+++ build/compile.sh
@@ -1,5 +1,5 @@
 #bitwise
-mpiFCCpx -Kparallel -Kfast -std=c++11 -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_bit
+mpiFCCpx -Kparallel -Kfast -std=c++11 -DENABLE_TIMING -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_bit
 
 #CPP set
-mpiFCCpx -Kparallel -Kfast -std=c++11 -DUSE_CPP_SET -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_set
+#mpiFCCpx -Kparallel -Kfast -std=c++11 -DUSE_CPP_SET -I../include ../src/main.cpp ../src/readFile.cpp ../src/fourHit.cpp -o run_set_normal
