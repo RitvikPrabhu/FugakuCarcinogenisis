f0c72079be6dd4b1f1c9988b31b62a1acf6fac41
Ritvik Prabhu <ritvikp@vt.edu>
2025-04-09 03:07:51 -0400

    2 hit added

--- include/twoHit.h
+++ include/twoHit.h
@@ -42,6 +42,6 @@ using LAMBDA_TYPE = long long;
 #endif
 
 void distribute_tasks(int rank, int size, const char *outFilename,
-                      const char *csvFileName, sets_t dataTable,
-                      double *omit_time);
+                      double elapsed_times[], sets_t dataTable);
+
 #endif
